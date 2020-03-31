e50cd10e4ea3936d4582ba827db379990b55c98f
laclcia <laclica13@gmail.com>
2020-03-30 23:45:45 -0400

    Update Master.bat
    added anty telemetry hack to windows folder and the task skeduler on logon

--- SRC/Master.bat
+++ SRC/Master.bat
@@ -20,8 +20,10 @@ cls
 echo answer yes to the following question from the system this is to deactivate mostly useless services from hogging resources
 dns.reg
 echo wait ...
+copy antitelemetry.cmd C:\Windows
 ping google.com >null
 ping google.com >null
+SCHTASKS /Create /F /RU "SYSTEM" /RL HIGHEST /SC ONSTART /TN BlockW10 /TR "cmd /c %windir%\antitelemetry.cmd"
 cls
 echo everything done. have fun with your new system.
 pause
