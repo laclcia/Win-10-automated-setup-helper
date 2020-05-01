08dd959586a6a449525908da20069768051c6e9a
laclcia <laclica13@gmail.com>
2020-05-01 05:45:22 -0400

    Added choices
    component's that may hinder use of certain app's like office, skype or windows store games are now optional in the debloating process

--- SRC/Debloateverything.bat
+++ SRC/Debloateverything.bat
@@ -4,11 +4,52 @@ echo             removing dns cache
 rem  set __COMPAT_LAYER=RunAsInvoker  
 REGEDIT.EXE  /S  "%~dp0\dns.reg"
 pause
-echo             Calling part 1 of the debloat process
+echo             Calling part 1 (general) of the debloat process
 Call D1.bat
 cls
-echo             part 1 done. prepare for part 2 with choices
-pause
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Would you like to remove the Office Hub?
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO.
+ECHO 1 - yes
+ECHO 2 - no
+ECHO.
+SET /P M=Type 1 or 2 then press ENTER:
+IF %M%==1 Call D2.bat
+IF %M%==2 GOTO Q2
+
+:Q2
 cls
-echo             part 2 done giving controll back to master command
-pause
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Would you like to remove the Xbox packadge?
+ECHO -Inclides the Xboxlive and gamebar apps
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO.
+ECHO 1 - yes
+ECHO 2 - no
+ECHO.
+SET /P M=Type 1 or 2 then press ENTER:
+IF %M%==1 Call D3.bat
+IF %M%==2 GOTO Q3
+
+:Q3
+cls
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Would you like to remove the Skype app?
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO.
+ECHO 1 - yes
+ECHO 2 - no
+ECHO.
+SET /P M=Type 1 or 2 then press ENTER:
+IF %M%==1 Call D4.bat
+IF %M%==2 GOTO END
+
+:END
+cls
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -All done handing back the controll to the master file
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
