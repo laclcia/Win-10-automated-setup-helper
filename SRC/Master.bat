089e90ea1c41d7eea89f9dee799b75f529da49de
laclcia <laclica13@gmail.com>
2020-05-01 05:56:35 -0400

    Added step text description to better guide users
    added some display text to better convey what is happening to the user.

--- SRC/Master.bat
+++ SRC/Master.bat
@@ -1,18 +1,47 @@
 @echo off
 cd %~dp0
+cls
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Welcome to the Master script. 
+ECHO -Step 1 Installing chocolatey
+ECHO -in this step we will install the chocolatey software to procede with
+ECHO -remote installations from the commandline
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping google.com >null
 call installchocolatey.bat
 echo  wait ...
 ping google.com >null
 pause 
 cls
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Step 2 Installing Softwares
+ECHO -in this step we will install your basic set of softwares
+ECHO -prepare for choice menues
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping google.com >null
 call installsoftwares.bat
 echo  wait ...
 ping google.com >null
 pause
+cls
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Step 3 Debloating Windows 10
+ECHO -in this step we will remove many of the default windows 10 bloatware
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping google.com >null
 call Debloateverything.bat
 echo wait ...
 ping google.com >null
 cls
+ECHO.
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ECHO -Step 4 System Optimisations
+ECHO -in this step we will Apply basic system optimisations
+echo -*-*-*-*-*-*-*-*-*-*-*-*--*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+ping google.com >null
 call W10optimisation.bat
 echo wait ...
 ping google.com >null
